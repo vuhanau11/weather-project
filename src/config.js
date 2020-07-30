1c759d8e3449e5b65393cf2a89a27ff4908dffea
tungnv <tungnvt1610@gmail.com>
2020-07-30 09:20:45 +0700

    fix UI

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  key: process.env.REACT_APP_API_KEY,
+  key: 'ed0d99a37fa4d76e73da1b594bc2ae9b',
   url: 'https://api.openweathermap.org/data/2.5/',
 };
 export default CONFIG;
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -5,20 +5,4 @@ export const searchWeather = async (place) => {
     `${CONFIG.url}weather?q=${place}&units=metric&appid=${CONFIG.key}`
   );
   return await response.json();
-
-  // try {
-  //   const response = await fetch(
-  //     `${CONFIG.url}weather?q=${place}&units=metric&appid=${CONFIG.key}`
-  //   );
-
-  //   const result = await response.json();
-  //   if (result.cod === 200) {
-  //     return result;
-  //   }
-  //   if (result.message) {
-  //     throw new Error(message);
-  //   }
-  // } catch (error) {
-  //   return { error };
-  // }
 };
